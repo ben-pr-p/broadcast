2ad367124ba595c99496227e0de457d4922961ac
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2015-11-28 13:01:21 -0600

    removed log messages

--- broadcast.js
+++ broadcast.js
@@ -31,11 +31,6 @@ function broadCastMany(message, teamsToSend, idx, fn) {
 }
 
 module.exports = function broadcast(data, fn) {
-  log(data);
-  log(teams);
-  log(data.team_domain);
-  log(teams[0].team_domain);
-  log(teams[0].team_domain == data.team_domain);
   var displayName = teams.filter(t => t.team_domain == data.team_domain)[0].team_display;
   var text = data.text.substr(10);
 
