ad7af5e58072afcb2a843808a701c2ab5e9fcb1c
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2015-11-28 11:18:58 -0600

    adding listening

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var querystring = require('querystring');
+var log = require('./debug')('csfb-broadcast')
+
+var app = express();
+app.use(bodyParser.json());
+
+var TEAM_TOKENS = process.env.TEAM_TOKENS;
+if (!TEAM_TOKENS) {
+  log('Missing env var TEAM_TOKENS – exiting...');
+  process.exit();
+}
+
+TEAM_TOKENS = TEAM_TOKENS.split(',');
+log('Starting with TEAM_TOKENS %s', TEAM_TOKENS);
+
+app.post('/', function (req, res) {
+  var data = req.body;
+  log('Recieved message with data %j', data);
+  res.send(200);
+});
+
+/*
+ * Get it started!
+ */ 
+
+var PORT = process.env.PORT;
+if (!PORT) {
+  log('Missing env var PORT, using 3000');
+  PORT = 3000;
+}
+
+log('Listening on PORT %d', PORT);
+app.listen(PORT);
