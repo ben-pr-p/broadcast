7608fa67e4680c38401cb3180dfe4e41b7cc6d1e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2015-12-16 21:40:15 -0600

    moved log messages around

--- db-api.js
+++ db-api.js
@@ -80,7 +80,7 @@ exports.addTeam = function addTeam (team, fn) {
           return fn(null, newTeam);
         });
       });
-      
+
     });
   });
 
@@ -103,7 +103,7 @@ exports.teamsFor = function (outTeamDomain, fn) {
       return t.accepts.indexOf(outTeamId) > -1;
     });
 
-    log('%s is allowed to send to %s', outTeam, teamsFor);
+    log('%s is allowed to send to %s', outTeam.id, teamsFor.id);
     return fn(null, teamsFor);
   });
 }
